3e1c008f51ffd39dd67dff88e9272b538578d020
SonuTheCode <sy2440052@gmail.com>
2026-04-11 11:30:45 +0530

    fix: resolved merge conflict

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,10 @@ app.use(cookieParser());
 
 //hosting ke baad origin mein url dena h taki auothize access na h0
 app.use(cors({
-  origin: "https://hackathon-project-9jun.onrender.com",
+  origin: [
+    "https://codewithsonu678.github.io",  
+    "http://127.0.0.1:5500"               
+  ],
   credentials: true
 }));
 
