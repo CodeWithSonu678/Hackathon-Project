cfa42c2e32bc0c4ab1f6761a4fcdf87148a1dcd0
SonuTheCode <sy2440052@gmail.com>
2026-04-11 17:43:58 +0530

    api fixed

--- backend/src/middleware/isAlreadyReg.js
+++ backend/src/middleware/isAlreadyReg.js
@@ -1,15 +1,34 @@
 const jwt = require("jsonwebtoken");
 const donorModel = require("../models/donor.model");
 async function isAlreadyReg(req,res,next){
-    const token = req.cookies.token;
+    // const token = req.cookies.token;
 
-    console.log(token);
+    // console.log(token);
     
-    if(!token){
-        return res.status(401).json({
-            msg:"Registration/Login is required",
-            success:false
-        })
+    // if(!token){
+    //     return res.status(401).json({
+    //         msg:"Registration/Login is required",
+    //         success:false
+    //     })
+    // }
+
+    let token;
+
+    // 1️⃣ check Authorization header
+    const authHeader = req.headers.authorization;
+
+    if (authHeader && authHeader.startsWith("Bearer ")) {
+      token = authHeader.split(" ")[1];
+    }
+
+    // 2️⃣ check cookies
+    if (!token && req.cookies.token) {
+      token = req.cookies.token;
+    }
+
+    // 3️⃣ no token
+    if (!token) {
+      return res.status(401).json({ msg: "Login required" });
     }
 
     try {
